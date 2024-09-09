5404290170942f54781b764247eaa2494de41c50
HBB <maxdo1579@mail.ru>
2024-09-09 18:52:10 +0300

    initial

---
+++ KPlugDI/src/main/java/ru/kiscode/kplugdi/KPlugDI.java
@@ -0,0 +1,17 @@
+package ru.kiscode.kplugdi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KPlugDI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
