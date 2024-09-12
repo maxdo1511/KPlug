047bc9d615f5bd30ae3731c84b298fabb1570cf9
HBB <maxdo1579@mail.ru>
2024-09-12 10:10:56 +0300

    Разработка BeanFactory надо переписать получение bean

--- KPlugDI/src/main/java/ru/kiscode/kplugdi/context/bean/BeanFactory.java
+++ KPlugDI/src/main/java/ru/kiscode/kplugdi/context/bean/BeanFactory.java
@@ -1,5 +1,7 @@
 package ru.kiscode.kplugdi.context.bean;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 import java.util.List;
 import java.util.Map;
 
@@ -9,13 +11,14 @@ public abstract class BeanFactory {
     private List<BeanPostProcessor> beanPostProcessors;
     private Map<String, Object> singletonObjectsByName;
     private Map<Class<?>, Object> singletonObjectsByClass;
+    private Map<String, Map<Class<?>, Object>> pluginObjectsByClass;
 
     public BeanFactory(Map<String, BeanDefinition> beanDefinitions, List<BeanPostProcessor> beanPostProcessors) {
         this.beanDefinitions = beanDefinitions;
         this.beanPostProcessors = beanPostProcessors;
     }
 
-    public <T> T getBean(Class<T> clazz) {
+    public <T> T getBean(Class<T> clazz, JavaPlugin plugin) {
         Object bean = singletonObjectsByClass.get(clazz);
         if (bean != null) {
             return (T) bean;
@@ -27,9 +30,17 @@ public abstract class BeanFactory {
         if (beanDefinition.getScope() == ScopeType.PROTOTYPE) {
             return (T) createBean(beanDefinition);
         }
+        if (beanDefinition.getScope() == ScopeType.PLUGIN) {
+            Object pluginBean = pluginObjectsByClass.get(plugin.getName()).get(clazz);
+            if (pluginBean != null) {
+                return (T) pluginBean;
+            }
+            Object pluginBeanInst = createBean(beanDefinition);
+            return (T) pluginBeanInst;
+        }
         for (Class<?> implementedInterface : beanDefinition.getImplementedInterfaces()) {
             if (clazz.equals(implementedInterface)) {
-                return (T) getBean(beanDefinition.getBeanClass());
+                return (T) getBean(beanDefinition.getBeanClass(), plugin);
             }
         }
         if (beanDefinition.getScope() == ScopeType.SINGLETON) {
@@ -48,8 +59,28 @@ public abstract class BeanFactory {
         if (beanDefinition == null) {
             throw new RuntimeException("Bean not found. Name: " + name);
         }
-        Object bean = createBean(beanDefinition);
-        return (T) bean;
+        if (beanDefinition.getScope() == ScopeType.PROTOTYPE) {
+            return (T) createBean(beanDefinition);
+        }
+        if (beanDefinition.getScope() == ScopeType.PLUGIN) {
+            Object pluginBean = pluginObjectsByClass.get(clazz);
+            if (pluginBean != null) {
+                return (T) pluginBean;
+            }
+            Object pluginBeanInst = createBean(beanDefinition);
+            pluginObjectsByClass.put(clazz, pluginBeanInst);
+            return (T) pluginBeanInst;
+        }
+        throw new RuntimeException("Bean not found. Name: " + name);
+    }
+
+    private <T> T getBeanImplementedInterface(Class<T> clazz, BeanDefinition beanDefinition, JavaPlugin plugin) {
+        for (Class<?> implementedInterface : beanDefinition.getImplementedInterfaces()) {
+            if (clazz.equals(implementedInterface)) {
+                return (T) getBean(beanDefinition.getBeanClass(), plugin);
+            }
+        }
+        return null;
     }
 
     public abstract void createBeans();
